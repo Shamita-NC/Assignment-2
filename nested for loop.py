d61441ca5b92a89710dbbb48c3b80d8cf9f54065
Shamita-NC <73642013+Shamita-NC@users.noreply.github.com>
2021-09-10 19:49:22 +0530

    Add files via upload

---
+++ nested for loop.py
@@ -0,0 +1,11 @@
+# Problem1
+a = '*'
+for i in range(1, 5):
+    for j in range(i):
+        print(a, end="")
+    print('')
+
+for i in range(5, 0, -1):
+    for j in range(i):
+        print(a, end='')
+    print('')
---
+++ reverse a word.py
@@ -0,0 +1,6 @@
+#Problem2
+
+a=input("Enter a word: ")
+rev=''.join(reversed(a))
+print("Output: ",rev)
+
